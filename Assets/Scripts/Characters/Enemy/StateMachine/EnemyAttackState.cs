4f91baacfdae175c4e1091bd3cc56597a97261da
camilohoyos16 <acamilohoyos@hotmail.com>
2020-04-01 07:23:32 -0500

    Added a random cd to enemies when they attack and deleted missign script in player prefab

--- Assets/Scripts/Characters/Enemy/StateMachine/EnemyAttackState.cs
+++ Assets/Scripts/Characters/Enemy/StateMachine/EnemyAttackState.cs
@@ -1,10 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class EnemyAttackState : EnemyState
 {
     public AttackComponent m_Attack;
+    public float attackCoolDown;
+
+    private Random randomCD;
+    private int maxCD;
+    private int minCD;
+
+    private void Start()
+    {
+        this.maxCD = 3;
+        this.maxCD = 1;
+        this.randomCD = new Random();
+        this.attackCoolDown = this.randomCD.Next(this.minCD, this.maxCD + 1);
+    }
 
     private void Attack()
     {
