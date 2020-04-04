8fada6a210e2d31bd4c3ae350b83b43ba9d67775
Cristian <crist.1707@hotmail.com>
2020-04-03 22:34:23 -0500

    Aim with mouse

--- Assets/Scripts/Characters/Player/PlayerController.cs
+++ Assets/Scripts/Characters/Player/PlayerController.cs
@@ -15,16 +15,18 @@ public class PlayerController : MonoBehaviour
     public PlayerActions Actions { get; set; }
     public bool usingKeyboard=false;
 
-
     [Header("Aim")]
     public GameObject aimGO;
     private float aimDistanceFromCenter = 3.5f;
     private Vector3 aimDirection;
+    private Vector3 mousePos;
+    public Camera cameraMain;
 
     void Awake()
     {
         this.rb = this.GetComponent<Rigidbody>();
         this.aimDirection = new Vector3(aimDistanceFromCenter,0,0);
+        this.cameraMain = Camera.main;
     }
 
     private void Start()
@@ -87,7 +89,7 @@ public class PlayerController : MonoBehaviour
         horizontal = this.Actions.move.X;
         vertical = this.Actions.move.Y;
         inputDirection = new Vector3(horizontal,0, vertical).normalized;
-        rb.velocity = inputDirection * speed * Time.deltaTime;
+        rb.velocity = inputDirection * speed * Time.fixedDeltaTime;
 
     }
     #endregion
@@ -95,17 +97,26 @@ public class PlayerController : MonoBehaviour
     #region Shoot and Aim
     private void Shoot()
     {
-        this.components.Attack.Attack(PowerType.BasicThrowBall, this.components, this.aimGO.transform.localPosition.normalized); //(playerController.aim.transform.position - transform.position).normalized
+        this.components.Attack.Attack(PowerType.BasicThrowBall, this.components, this.aimDirection.normalized); //(playerController.aim.transform.position - transform.position).normalized
         Debug.LogError("Shoot");
     }
     private void Aiming()
     {
-        this.aimDirection = new Vector3(this.Actions.aimMove.X, 0, this.Actions.aimMove.Y);
 
-        if (this.aimDirection.magnitude > 0.5f) {
-            this.aimDirection.Normalize();
-            this.aimDirection *= aimDistanceFromCenter;
-            this.aimGO.transform.localPosition = this.aimDirection;
+        if (usingKeyboard) {
+            mousePos = cameraMain.ScreenToWorldPoint(Input.mousePosition);
+
+            this.aimGO.transform.localPosition = Vector3.ClampMagnitude(mousePos,aimDistanceFromCenter); // aim clamped 
+            //this.aimGO.transform.localPosition = mousePos; // free aim
+            this.aimDirection = this.aimGO.transform.localPosition;
+        } else {
+
+            this.aimDirection = new Vector3(this.Actions.aimMove.X, 0, this.Actions.aimMove.Y);
+            if (this.aimDirection.magnitude > 0.5f) {
+                this.aimDirection.Normalize();
+                this.aimDirection *= aimDistanceFromCenter;
+                this.aimGO.transform.localPosition = this.aimDirection;
+            }
         }
     }
     #endregion
