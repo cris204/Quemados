698702256de803a31e661542c29942b28eb8782a
Cristian <crist.1707@hotmail.com>
2020-04-04 21:41:07 -0500

    menu

---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public void LoadGame()
+    {
+        OwnSceneLoadManager.Instance.LoadScene("GameScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
