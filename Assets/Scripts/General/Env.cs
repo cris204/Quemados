6cf2898ba266b350cf991edb085df2854bcf0b25
Cristian <crist.1707@hotmail.com>
2020-04-24 19:00:18 -0500

    Fixed initial enemies in round

--- Assets/Scripts/General/Env.cs
+++ Assets/Scripts/General/Env.cs
@@ -5,7 +5,7 @@
 
     //Enemies
     public static int MAX_ENEMIES_ALIVE_COUNT = 20;
-    public static int INITIAL_ENEMIES_ROUND = 1;
+    public static int INITIAL_ENEMIES_ROUND = 10;
     public static int PLUS_ENEMIES_PER_ROUND = 2;
 
     //Rounds
